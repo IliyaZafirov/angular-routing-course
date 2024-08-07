5b7f84340208067cc33b2f28bac7fb71a92874f0
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-08-07 19:24:56 +0300

    initial boilerplate of singleton module four

--- src/app/combo-card-view/combo-card-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-combo-card-view',
-  templateUrl: './combo-card-view.component.html',
-  styleUrls: ['./combo-card-view.component.css']
-})
-export class ComboCardViewComponent {
-}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 import { Category } from '../models/pie';
 import { PieService } from '../services/pie.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [
+    MatMenuModule,
+    MatButtonModule,
+    RouterLink
+  ],
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
